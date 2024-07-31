0d6a44bb699cf3e410ab81936a90c9998dbdfc1a
Ian Rodrigues de Castro Silva <ianrcsd@gmail.com>
2024-07-31 10:04:03 +0200

    Add mommon methods

--- common/data_treatment.py
+++ common/data_treatment.py
@@ -6,22 +6,24 @@ def treat_general_data(df) -> pd.DataFrame:
     df.dropna(axis=1, how='all', inplace=True)
     df.fillna(pd.NA, inplace=True) 
     df = df.apply(lambda col: col.map(lambda x: x.strip() if isinstance(x, str) else x)).replace('', pd.NA)
+    df = df.where(pd.notnull(df), None)
     df.drop_duplicates(inplace=True)
-
     return df
 
 def convert_columns_to_datetime(df, columns) -> pd.DataFrame:
     for column in columns:
-        df[column] = pd.to_datetime(df[column], format='%Y-%m-%dT%H:%M:%S', errors='coerce')
+        df[column] = pd.to_datetime(df[column], format='%Y-%m-%d %H:%M:%S', errors='coerce')
     return df
 
 
 def treat_country_code_data(df: pd.DataFrame, country_code_column: str) -> pd.DataFrame:
+
     def is_valid_country_code(code):
         try:
             code = code.upper()
             return pycountry.countries.get(alpha_2=code) is not None
         except AttributeError:
             return False
-    df[country_code_column] = df[country_code_column].map(lambda code: code if is_valid_country_code(code) else "OTHER")
+        
+    df[country_code_column] = df[country_code_column].apply(lambda code: code if is_valid_country_code(code) else "OTHER")
     return df
